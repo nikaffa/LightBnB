2e9f96106f96cd505e72ef32641f97a8a5525768
Veronika Fatikhova <fidelio@list.ru>
2021-11-24 15:07:07 -0800

    tables and seeds modified

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,9 +9,9 @@ VALUES (1, 1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/208
 (3, 3, 'Bear corner', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true);
 
 INSERT INTO reservations 
-VALUES (1, 1, 1, '2018-09-11', '2018-09-26'),
-(2, 2, 2, '2018-09-11', '2018-09-26'),
-(3, 3, 3, '2019-01-04', '2019-02-01');
+VALUES (1, '2018-09-11', '2018-09-26', 1, 1),
+(2, '2018-09-11', '2018-09-26', 2, 2),
+(3, '2019-01-04', '2019-02-01', 3, 3);
 
 INSERT INTO property_reviews
 VALUES (1, 1, 1, 1, 5, 'message'),
